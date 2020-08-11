db6670728ef3e020a9a628b5d79f12c89b4d00aa
lustrewang <lustrewang@tencent.com>
2020-08-11 20:10:19 +0800

    add login html

--- js/background.js
+++ js/background.js
@@ -1,5 +1,14 @@
 chrome.app.runtime.onLaunched.addListener(function () {
-    chrome.app.window.create('window.html', {
+    chrome.browserAction.onClicked.addListener(function(tab){  
+        chrome.app.window.create('window.html', {
+            'outerBounds': {
+                'width': 1000,
+                'height': 720
+            }
+        });
+    });
+    
+    chrome.app.window.create('login.html', {
         'outerBounds': {
             'width': 1000,
             'height': 720
